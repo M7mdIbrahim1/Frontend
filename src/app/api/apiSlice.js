962b40c37714b2d755463563337ed9e2f7f4724f
M7mdIbrahim1 <66166461+M7mdIbrahim1@users.noreply.github.com>
2023-09-20 18:21:49 +0300

    first publish

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials, logOut } from '../../features/auth/slices/authSlice'
 import { selectCurrentToken} from "../../features/auth/slices/authSlice"
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://3.71.175.227/api',
+    baseUrl: 'http://3.75.83.162/api',
   // baseUrl: 'https://leveragefc-backend.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
